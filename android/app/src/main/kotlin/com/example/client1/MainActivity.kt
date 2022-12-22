7b867ef9dc73b0b0e27077bc0e21327d3c0a0f42
Tho Le <thodoanle26@gmail.com>
2022-12-22 21:49:26 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/client1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.client1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/globals.dart
@@ -0,0 +1,32 @@
+import 'package:client1/settings_dialog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum SourceOption { gallery, camera }
+
+class Global {
+  SourceOption srcOption = SourceOption.gallery;
+
+  SettingsData settings = SettingsData(
+      contactName: "", homeAddress: "", phoneNumber: "", apiAddress: "");
+
+  String prediction = '';
+  Map<String, dynamic> jsonItems = {};
+
+  Future<void> initSettingsDialog() async {
+    final prefs = await SharedPreferences.getInstance();
+    settings.contactName = prefs.getString('contactName') ?? "";
+    settings.homeAddress = prefs.getString('homeAddress') ?? "";
+    settings.phoneNumber = prefs.getString('phoneNumber') ?? "";
+    settings.apiAddress = prefs.getString('apiAddress') ?? "";
+  }
+
+  void globalMountSettings(SettingsData _settings) {
+    settings = _settings;
+  }
+
+  String fullNameOfDisease() {
+    return jsonItems[prediction]['name'];
+  }
+}
+
+Global global = Global();
